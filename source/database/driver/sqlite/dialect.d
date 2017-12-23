0979badc35c7462a8b95f3227d3367db8151cb47
viile <zhudj@putao.com>
2017-12-23 15:56:48 +0800

    compare func

--- source/database/driver/sqlite/dialect.d
+++ source/database/driver/sqlite/dialect.d
@@ -6,18 +6,38 @@ class SqliteDialect : Dialect
 {
 	string closeQuote() { return `"`; }
 	string openQuote()  { return `"`; }
-    string toSqlValueImpl(DlangDataType type,Variant value)
+
+	Variant fromSqlValue(DlangDataType type,Variant value)
+	{
+		if(typeid(type) == typeid(dBoolType)){
+			if(*value.peek!string == "1")
+				return Variant(true);
+			else
+				return Variant(false);
+		}else if(typeid(type) == typeid(dFloatType))
+			return Variant(safeConvert!(string,float)(*value.peek!string));
+		else if(typeid(type) == typeid(dDoubleType))
+			return Variant(safeConvert!(string,double)(*value.peek!string));
+		else if(typeid(type) == typeid(dIntType))
+			return Variant(safeConvert!(string,int)(*value.peek!string));
+		else if(typeid(type) == typeid(dLongType))
+			return Variant(safeConvert!(string,long)(*value.peek!string));
+		else
+			return value;
+	}
+
+	string toSqlValueImpl(DlangDataType type,Variant value)
 	{
 		if(typeid(type) == typeid(dBoolType))
-				return value.get!(bool) ? "1" : "0";
+			return value.get!(bool) ? "1" : "0";
 		else if(typeid(type) == typeid(dFloatType))
-				//return isNaN(*value.peek!float) ? "0" : *value.peek!string;
-                return (*value.peek!float).to!string;
+			//return isNaN(*value.peek!float) ? "0" : *value.peek!string;
+			return (*value.peek!float).to!string;
 		else if(typeid(type) == typeid(dDoubleType))
-				//return isNaN(*value.peek!double) ? "0" : *value.peek!string;
-                return (*value.peek!double).to!string;
+			//return isNaN(*value.peek!double) ? "0" : *value.peek!string;
+			return (*value.peek!double).to!string;
 		else if(typeid(type) == typeid(dIntType))
-				return value.toString;
+			return value.toString;
 		else
 			return openQuote ~ value.toString ~ closeQuote;
 	}
@@ -25,7 +45,7 @@ class SqliteDialect : Dialect
 
 string[] SQLITE_RESERVED_WORDS = 
 [
-	"ABORT",
+"ABORT",
 	"ACTION",
 	"ADD",
 	"AFTER",
@@ -146,4 +166,4 @@ string[] SQLITE_RESERVED_WORDS =
 	"VIRTUAL",
 	"WHEN",
 	"WHERE",
-];
+	];
