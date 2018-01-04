13e71ce8382200ef2257567326a5b315912438d5
viile <dakgzhu@foxmail.com>
2018-01-04 14:33:28 +0800

    fixed caid

--- source/database/database.d
+++ source/database/database.d
@@ -15,56 +15,78 @@ import database;
 
 class Database
 {
-    Pool _pool;
-    DatabaseOption _options;
+	Pool _pool;
+	DatabaseOption _options;
 
-    this(string url)
-    {
-        this._options = new DatabaseOption(url);
-        initPool();
-    }
+	this(string url)
+	{
+		this._options = new DatabaseOption(url);
+		initPool();
+	}
 
-    this(DatabaseOption options)
-    {
-        this._options = options;
-        initPool();
-    }
+	this(DatabaseOption options)
+	{
+		this._options = options;
+		initPool();
+	}
 
-    private void initPool()
-    {
-        _pool = new Pool(this._options);
-    }
+	private void initPool()
+	{
+		_pool = new Pool(this._options);
+	}
 
-    Transaction beginTransaction()
-    {
-        Connection _conn = _pool.getConnection();
+	Transaction beginTransaction()
+	{
+		Connection _conn = _pool.getConnection();
 		Transaction tran = new Transaction(this,_pool,_conn);
 		tran.begin();
 		return tran;
-    }
+	}
 
-    int error()
-    {
-        return 0;
-    }
+	int error()
+	{
+		return 0;
+	}
 
-    int execute(string sql)
-    {
-        return new Statement(_pool, sql).execute();
-    }
+	int execute(string sql)
+	{
+		return new Statement(_pool, sql).execute();
+	}
 
-    ResultSet query(string sql)
-    {
-        return (new Statement(_pool, sql)).query();
-    }
+	ResultSet query(string sql)
+	{
+		return (new Statement(_pool, sql)).query();
+	}
 
-    Statement prepare(string sql)
-    {
-        return new Statement(_pool, sql);
-    }
+	Statement prepare(string sql)
+	{
+		return new Statement(_pool, sql);
+	}
 
-    void close()
-    {
+	void close()
+	{
 		_pool.close();	
-    }
+	}
+
+	SqlBuilder createSqlBuilder()
+	{
+		return (new SqlFactory()).createMySqlBuilder();
+	}
+
+	Dialect createDialect()
+	{
+		version(USE_MYSQL){
+			return new MysqlDialect();
+		}
+        else version(USE_POSTGRESQL)
+		{
+			return new PostgresqlDialect(); 
+		}
+		else version(USE_SQLITE)
+		{
+			return new SqliteDialect(); 
+		}
+        else
+			throw new DatabaseException("Unknow Dialect");
+	}
 }
