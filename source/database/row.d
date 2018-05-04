a5eba7d732e2296540b8deb75d299c52a493ec59
noclear <noclear@163.com>
2018-05-04 17:15:58 +0800

    support join function

--- source/database/row.d
+++ source/database/row.d
@@ -12,12 +12,38 @@
 module database.row;
 
 import database;
+import std.algorithm.comparison;
+import std.regex;
+
+
+
+
+public class RowDataS {
+	string name;
+	string value;
+	TypeInfo type;
+} 
+
+public class RowData {
+	RowDataS[string] _data;
+	void addData(string name, RowDataS data) {
+		_data[name] = data;
+	}
+	RowDataS getData(string name) {
+		return _data.get(name, null);
+	}
+	RowDataS[string] getAllData() {return _data;}
+}
 
 class Row 
 {
 	private ResultSet _resultSet;
 	private string[string] vars;
 	private TypeInfo[string] types;
+
+	RowData[string] rowData;
+
+	private int count;
 
 	this(ResultSet resultSet)
 	{
@@ -37,13 +63,27 @@ class Row
 
 	void add(string name,TypeInfo type,string val)
 	{
-		if (name in vars)
+		if (name in vars) 
 			throw new DatabaseException("field "~name~" exits");
 		vars[name] = val;
 		types[name] = type;
+		auto nameFields = split(name, "__");
+		if (nameFields.length == 3 && nameFields[1] == "as") {
+			RowDataS data = new RowDataS;
+			data.type = type;
+			data.value = val;
+			if (nameFields[0] !in rowData)
+				rowData[nameFields[0]] = new RowData();
+			rowData[nameFields[0]].addData(nameFields[2], data); 
+		}
 	}
 
-	string opDispatch(string name,string file = __FILE__,int line = __LINE__)()
+	RowData getAllRowData(string table) {
+		return rowData.get(table, null);
+	}
+
+
+	string opDispatch(string name, string file = __FILE__,int line = __LINE__)()
 	{
 		if(name !in vars)
 			throw new DatabaseException("no field "~name~" in result", file, line);
